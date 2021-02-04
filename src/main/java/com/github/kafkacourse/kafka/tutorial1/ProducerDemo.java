bc4b182e1376fb9b93eed7f069515fe0c647f74e
Diego Pereira <diego.pereira@farfetch.com>
2021-02-04 22:30:33 +0000

    Initial commit

---
+++ src/main/java/com/github/kafkacourse/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,31 @@
+package com.github.kafkacourse.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world!");
+
+        // Send data
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
